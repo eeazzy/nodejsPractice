f43d5039d29cdfac250905048068b587ca6b2910
eeazzy <khs2498@gmail.com>
2024-08-04 18:05:54 +0900

    html 불러오기

--- node2/express_server1/app.js
+++ node2/express_server1/app.js
@@ -1,9 +1,10 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('hi');
+    res.sendFile(path.join(__dirname,'./index.html'));
 });
 
 app.listen(8080, () => {
